13c1a69e0c4ac12a92c94666c35fcb1cd0949a2f
iamguruprasath-v <guruprasathv.dev@gmail.com>
2025-05-23 15:37:24 +0530

    Reading feature enabled

--- script.js
+++ script.js
@@ -2,8 +2,50 @@ import utils from './utils.js';
 
 const addNewButton = document.getElementById('add-new');
 const tableBody = document.getElementById('task-table-content');
+const toggleStatus = document.getElementById('toggle-status');
+const toggleBtn = document.getElementById('toggle-button');
+const taskInput = document.getElementById('task-input');
 
-addNewButton.addEventListener('click', (e) => {
-    // document.querySelector('.container').classList.add("blurred");
-    console.log(e.target);
-})
+let allTasks = utils.getAllTasks();
+console.log(allTasks);
+renderAllTasks(allTasks);
+
+function renderAllTasks(tasks) {
+    for (let loopIndex = 0; loopIndex < tasks.length; loopIndex++) {
+        let task = tasks[loopIndex];
+        let taskRow = utils.createNewElement('tr');
+        let taskId = utils.createNewElement('td', [], loopIndex+1);
+        let taskName = utils.createNewElement('td', [], task.taskName);
+        let taskStatus = utils.createNewElement('td');
+        taskStatus.innerHTML = task.status === 'true' ? '<div class="toggle" id="toggle-status"><div class="toggle-button" data-status = "true"></div></div>' : '<div class="toggle" id="toggle-status"><div class="toggle-button" data-status = "false"></div></div>';
+        let actionButtons = utils.createNewElement('td', ['actions']);
+        utils.setActionButtons(actionButtons);
+        taskRow.appendChild(taskId);
+        taskRow.appendChild(taskName);
+        taskRow.appendChild(taskStatus);
+        taskRow.appendChild(actionButtons);
+        tableBody.insertAdjacentElement('afterbegin', taskRow);
+    }
+}
+
+let isToggled = false;
+
+taskInput.addEventListener('focus', () => {
+    taskInput.setAttribute('placeholder', 'Enter task name');
+
+});
+taskInput.addEventListener('blur', () => {
+    taskInput.setAttribute('placeholder', 'Click here - Task creation');
+});
+
+toggleStatus.addEventListener('click', () => {
+    isToggled = !isToggled;
+  
+    if (isToggled) {
+      toggleBtn.style.transform = 'translateX(20px)';
+      toggleStatus.style.backgroundColor = 'grey';
+    } else {
+      toggleBtn.style.transform = 'translateX(0)';
+      toggleStatus.style.background = 'transparent';
+    }
+  });
--- utils.js
+++ utils.js
@@ -13,7 +13,35 @@ function createNewElement(element, classes = [], text = "") {
     return elem;
 }
 
+function setActionButtons(parent) {
+    let addButton = createNewElement('button');
+    let editButton = createNewElement('button');
+    let deleteButton = createNewElement('button');
+    addButton.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    editButton.innerHTML = '<i class="fa-duotone fa-solid fa-circle-check"></i>'
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    parent.appendChild(addButton);
+    parent.appendChild(editButton);
+    parent.appendChild(deleteButton);
+}
+
+function getAllTasks() {
+    let tasks = [];
+    let taskCount = localStorage.length;
+
+    for (let i = 0; i < taskCount; i++) {
+        let task = {};
+        console.log(localStorage.key(i));
+        task.taskName = localStorage.key(i);
+        task.status = localStorage.getItem(task.taskName);
+        tasks.push(task);
+    }
+
+    return tasks;
+}
 
 export default {
     createNewElement,
+    setActionButtons,
+    getAllTasks
 }
